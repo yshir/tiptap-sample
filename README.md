cbb00bd0fd878794b7a5768595475cbbc46df074
yshir <yshir0020@gmail.com>
2022-09-18 16:57:24 +0900

    feat: add sample code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tiptap-sample
 
 - tiptap: https://tiptap.dev/
+
+![localhost_5173_](https://user-images.githubusercontent.com/22204755/190891940-60ce2f7b-d910-4602-85e3-3c74a9a5637e.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Tiptap from './Tiptap';
 
 function App() {
-  return <div className="App">Hello, World</div>;
+  return (
+    <div className="App">
+      <Tiptap />
+    </div>
+  );
 }
 
 export default App;
